4cc3b3636aee6a3600ece9a5dd3d008523365ea5
Rhouma Mohamed <medrhouma1234@gmail.com>
2024-10-10 10:59:49 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,43 @@
+// src/app/app.component.ts
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { FaceSnapDSI22Component } from './face-snap-dsi22/face-snap-dsi22.component';
 import { FaceSnap } from './models/face-snap.model';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FaceSnapDSI22Component],
+  imports: [RouterOutlet, FaceSnapDSI22Component, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit  {
-  mySnap!: FaceSnap; 
-  ngOnInit(){
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon Meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
+export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
 
-    );
+  ngOnInit() {
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date('2024-10-10'), // Date spécifique pour tester
+        snaps: 0
+      },
+      {
+        title: '3ojja mergez',
+        description: 'yummmmm yummm yummm.',
+        imageUrl: 'https://th.bing.com/th/id/OIP.er76B93MYyzLPtpE-_BgewHaJD?rs=1&pid=ImgDetMain',
+        createdDate: new Date('2024-10-10'),
+        snaps: 0
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Yummm, c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date('2024-10-10'),
+        snaps: 0
+      }
+    ];
   }
 }
--- src/app/face-snap-dsi22/face-snap-dsi22.component.ts
+++ src/app/face-snap-dsi22/face-snap-dsi22.component.ts
@@ -1,33 +1,36 @@
-import { Component, Input } from '@angular/core';
+// src/app/face-snap-dsi22/face-snap-dsi22.component.ts
+import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
-
+import { NgClass, DatePipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap-dsi22',
   standalone: true,
-  imports: [],
+  imports: [NgClass, DatePipe, CommonModule],
   templateUrl: './face-snap-dsi22.component.html',
-  styleUrl: './face-snap-dsi22.component.css'
+  styleUrls: ['./face-snap-dsi22.component.css']
 })
-export class FaceSnapDSI22Component {
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
+export class FaceSnapDSI22Component implements OnInit {
   buttonText!: string;
-  @Input() faceSnap!:FaceSnap;
+  snapEmoji = '🤌';
+  @Input() faceSnap!: FaceSnap;
+
   ngOnInit() {
- this.buttonText = 'Oh Snap!';
-}
-onSnap() {
-  if (this.buttonText === 'Oh Snap!') {
-    this.faceSnap.snaps++;
-    this.buttonText = 'Oops, un Snap!';
-  } else {
-    this.faceSnap.snaps--;
     this.buttonText = 'Oh Snap!';
+
+    // Assurez-vous que createdDate est une instance de Date
+    if (!(this.faceSnap.createdDate instanceof Date)) {
+      this.faceSnap.createdDate = new Date(this.faceSnap.createdDate);
+    }
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.faceSnap.snaps++;
+      this.buttonText = 'Oops, un Snap!';
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
   }
 }
-}
-
